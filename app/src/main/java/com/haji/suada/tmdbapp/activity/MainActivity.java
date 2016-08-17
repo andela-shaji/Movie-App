a4c2dae1d34cc82f214b414b64842606cb44aed1
Suada Haji <suadahaji@Suadas-MacBook-Pro-2.local>
2016-08-17 11:52:45 +0300

    Add movie class to the model package

--- app/src/main/java/com/haji/suada/tmdbapp/activity/MainActivity.java
+++ app/src/main/java/com/haji/suada/tmdbapp/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.haji.suada.tmdbapp;
+package com.haji.suada.tmdbapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.haji.suada.tmdbapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
