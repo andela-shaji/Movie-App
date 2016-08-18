3e7e88c38560daade26cd03be7efbc40c57587d9
Suada Haji <suadahaji@Suadas-MacBook-Pro-2.local>
2016-08-18 09:39:51 +0300

    Define the endpoints and make the first request

--- app/src/main/java/com/haji/suada/tmdbapp/activity/MainActivity.java
+++ app/src/main/java/com/haji/suada/tmdbapp/activity/MainActivity.java
@@ -2,14 +2,49 @@ package com.haji.suada.tmdbapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.haji.suada.tmdbapp.R;
+import com.haji.suada.tmdbapp.model.Movie;
+import com.haji.suada.tmdbapp.model.MovieResponse;
+import com.haji.suada.tmdbapp.rest.ApiClient;
+import com.haji.suada.tmdbapp.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private final static String API_KEY = "5ab68c282365772dba538bd0db9f5fda";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
+        ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MovieResponse> call = apiInterface.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                Log.d(TAG, "Number of movies received: " + movies.size());
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Log.e(TAG, t.toString());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/haji/suada/tmdbapp/rest/ApiInterface.java
@@ -0,0 +1,33 @@
+package com.haji.suada.tmdbapp.rest;
+
+import com.haji.suada.tmdbapp.model.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by suadahaji.
+ */
+public interface ApiInterface {
+
+    /**
+     * @Query – specifies the query key name with the value of the annotated parameter.
+     */
+    @GET("movie/top_rated")
+    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+
+    /**
+     * @Path – variable substitution for the API endpoint.
+     * For example movie id will be swapped for{id} in the URL endpoint.
+     *
+     * @Body – payload for the POST call
+     *
+     * @Header – specifies the header with the value of the annotated parameter
+     */
+    @GET("movie/{id}")
+    Call<MovieResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+
+}
