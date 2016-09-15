b9e9dace4c527ec257c7af6210d0cda8ad370029
“Suada <suada.haji@andela.com>
2016-09-15 22:28:57 +0300

    Add Toolbar to the Movies Activity

--- app/src/main/java/com/haji/suada/tmdbapp/activity/MoviesActivity.java
+++ app/src/main/java/com/haji/suada/tmdbapp/activity/MoviesActivity.java
@@ -2,15 +2,24 @@ package com.haji.suada.tmdbapp.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.haji.suada.tmdbapp.R;
 
 public class MoviesActivity extends AppCompatActivity {
+  private Toolbar toolbar;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
     setContentView(R.layout.activity_movies);
+
+    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setTitle("Movies");
+
+
+
   }
 }
